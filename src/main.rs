7ce68d4349fdf441625163f2ab7aeb75678aa69d
MohsenBg <moh.1380.1393@gmail.com>
2023-06-21 09:05:56 +0330

    File '"2023-06-21_09-05-55.log"' has been created.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use dotenv::dotenv;
-use std::{env, path::PathBuf, process};
+use std::{path::PathBuf, process};
 mod command;
 mod errors;
 mod git_util;
@@ -8,27 +7,6 @@ use errors::ExitCode;
 use git_util::{send_to_git_hub, GitOptions};
 
 fn main() {
-    //example
-
-    if let Err(err) = dotenv() {
-        println!("Error loading .env file: {}", err);
-        panic!(
-            "An error occurred. Error code: {}",
-            ExitCode::EnvFailed as i32
-        );
-    }
-
-    let ssh_password = match env::var("SSH_GIT_PASSWORD") {
-        Ok(password) => password,
-        Err(_) => {
-            println!("Failed to retrieve SSH_GIT_PASSWORD");
-            panic!(
-                "An error occurred. Error code: {}",
-                ExitCode::ReadShhPasswordFailed as i32
-            );
-        }
-    };
-
     let mut git_option = GitOptions {
         repo_directory: PathBuf::from("/home/Bagheri/Projects/rust/devWorker/"),
         commit_msg: String::new(),
@@ -47,27 +25,4 @@ fn main() {
         Ok(()) => process::exit(ExitCode::Success as i32),
         Err(err) => panic!("{} Error code: {}", err, 1),
     }
-
-    // match command::check_git_installed() {
-    //     Ok(msg) => println!("{}", msg),
-    //     Err(err) => panic!("{} Error code: {}", err, ExitCode::GitNotFound as i32),
-    // }
-
-    // match command::git_add() {
-    //     Ok(msg) => println!("{}", msg),
-    //     Err(err) => panic!("{} Error code: {}", err, ExitCode::GitAddFailed as i32),
-    // }
-
-    // match command::git_commit(format!(
-    //     "File '{:?}' has been created.",
-    //     file.file_name().unwrap()
-    // )) {
-    //     Ok(msg) => println!("{}", msg),
-    //     Err(err) => panic!("{} Error code: {}", err, ExitCode::GitAddFailed as i32),
-    // }
-
-    // match command::git_push("MohsenBg", &ssh_password) {
-    //     Ok(msg) => println!("{}", msg),
-    //     Err(err) => panic!("{} Error code: {}", err, ExitCode::GitAddFailed as i32),
-    // }
 }
